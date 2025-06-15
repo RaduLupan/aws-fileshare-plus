5edf82502286cf79d38685d9d71d6196ae4a7cdc
Radu Lupan <github@lupan.ca>
2025-06-14 20:39:28 -0400

    Added output for ecs_cluster_name

--- terraform/modules/ecs-flask-backend/outputs.tf
+++ terraform/modules/ecs-flask-backend/outputs.tf
@@ -15,6 +15,11 @@ output "ecs_cluster_id" {
   value       = aws_ecs_cluster.this.id
 }
 
+output "ecs_cluster_name" {
+  description = "The name of the ECS cluster."
+  value       = aws_ecs_cluster.this.name
+}
+
 output "ecs_service_name" {
   description = "The name of the ECS service."
   value       = aws_ecs_service.this.name
