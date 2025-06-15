c2fb12392c36aa3f221a41edeb97ee7050eddb19
Radu Lupan <github@lupan.ca>
2025-06-15 10:47:30 -0400

    Removed output temporarely.

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -88,11 +88,11 @@ output "s3_react_bucket_name" {
   value       = module.frontend_app.s3_bucket_name
 }
 
-output "backend_alb_url" {
+/* output "backend_alb_url" {
   description = "The URL of the backend ALB."
   value       = "http://${module.backend_app.alb_dns_name}" # Use http for now, update to https when ALB has certificate
 }
-
+ */
 output "s3_uploads_bucket_name" {
   description = "The S3 bucket name for file uploads."
   value       = module.backend_app.uploads_s3_bucket_name
