2e7cd5cd98155389e3ed0c51a07b8db592d104d9
Radu Lupan <github@lupan.ca>
2025-06-16 07:17:28 -0400

    Fixed attribute name

--- terraform/modules/cognito-user-management/main.tf
+++ terraform/modules/cognito-user-management/main.tf
@@ -93,7 +93,8 @@ resource "aws_lambda_function" "post_confirmation_trigger" {
 resource "aws_lambda_permission" "allow_cognito" {
   statement_id  = "AllowCognitoInvoke"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.post_confirmation_trigger.name
+  # CORRECTED: The attribute is 'function_name', not 'name'.
+  function_name = aws_lambda_function.post_confirmation_trigger.function_name
   principal     = "cognito-idp.amazonaws.com"
   source_arn    = aws_cognito_user_pool.this.arn
 }
